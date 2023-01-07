8172fc37fc8d52600932aa99dc049d98df9b346f
ibrahim <ibrahimkhalaf99@gmail.com>
2023-01-07 15:12:32 +0200

    Create admin panel routes

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,32 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CastController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\SerieController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Livewire\CastIndex;
+use App\Http\Livewire\EpisodeIndex;
+use App\Http\Livewire\GenreIndex;
+use App\Http\Livewire\MovieIndex;
+use App\Http\Livewire\SeasonIndex;
+use App\Http\Livewire\SerieIndex;
+use App\Http\Livewire\TagIndex;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [WelcomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('movies', MovieIndex::class)->name('movies.index');
+    Route::get('series', SerieIndex::class)->name('series.index');
+    Route::get('series/{serie}/seasons', SeasonIndex::class)->name('seasons.index');
+    Route::get('series/{serie}/seasons/{season}/episodes', EpisodeIndex::class)->name('episodes.index');
+    Route::get('casts', CastIndex::class)->name('casts.index');
+    Route::get('tags', TagIndex::class)->name('tags.index');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
