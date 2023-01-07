915fbd9f5f745fc06e880ef9da3761150ed153d6
ibrahim <ibrahimkhalaf99@gmail.com>
2023-01-07 15:27:56 +0200

    Adjusted database migration files

---
+++ database/seeders/UserTabelSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserTabelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
